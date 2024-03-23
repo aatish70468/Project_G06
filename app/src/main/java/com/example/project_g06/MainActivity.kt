a88a89f902a5be4d427e32f7d377a7cf19ae83f7
aatish70468 <amp70468@gmail.com>
2024-03-23 17:53:49 -0400

    latestchange

--- app/src/main/java/com/example/project_g06/MainActivity.kt
+++ app/src/main/java/com/example/project_g06/MainActivity.kt
@@ -10,6 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
+
+
+        
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
